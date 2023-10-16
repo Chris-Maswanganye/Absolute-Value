4b8ffe81f901d47c1c2a4472fe1351f27d1435b6
Dell-User <Dell-User@DESKTOP-BSDRSH6>
2023-10-16 11:15:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absolute_Value
+{
+    internal class Program
+    { 
+            static int AbsoluteValue(int num)
+            {
+                return num < 0 ? -num : num;
+            }
+
+            static void Main()
+            {
+                int number1 = 6832;
+                int result1 = AbsoluteValue(number1);
+                Console.WriteLine(result1);
+
+                int number2 = -392;
+                int result2 = AbsoluteValue(number2);
+                Console.WriteLine(result2);
+            }
+        }
+
+    }
